1872dd6956b88f33386b343bfb452cee2df80e2c
kuutlhannnkp <52879315+kuutlhannnkp@users.noreply.github.com>
2023-08-02 01:48:01 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const todoInput = document.getElementById("todoText");
+const submitTodo = document.getElementById("submitTodo");
+submitTodo.addEventListener("click", addTodo);
+const todosList = document.getElementById("todos_list");
+
+function addTodo() {
+  if (todoInput.value === "") {
+    alert("You must write Something!!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = todoInput.value;
+    todosList.appendChild(li);
+    todoInput.value = "";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  todosList.addEventListener("click", function (e) {
+    var clickedTodo = e.target;
+    if (clickedTodo.classList.contains("clicked") === false) {
+      firstClick();
+    } else {
+      secondClick();
+    }
+
+    function firstClick() {
+      clickedTodo.style.textDecoration = "line-through";
+      clickedTodo.classList.add("clicked");
+    }
+    function secondClick() {
+      clickedTodo.remove();
+    }
+  });
+});
